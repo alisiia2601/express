4c4e8ca65cd90673344590d99bd9d576c7e7b49e
alisiia2601 <alisiia.ryzhykova@icloud.com>
2023-05-17 16:10:26 +0200

    fixes

--- index.js
+++ index.js
@@ -1,26 +1,20 @@
 const express = require('express');
 const moviesRouter = require('./routes/movies');
-const authentification = require("./apiKey")
+const authentication = require('./apiKey');
+const addApiKey = require('./apiKey');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 
-app.use("/addApiKey", authentification)
-
-app.use((req, res, next) => {
-  authentification(req, res, next);
-})
-
-app.get("/", (req, res) => {
-  res.send("Hello!");
+app.get('/', (req, res) => {
+  res.send('Hello!');
 });
 
+app.use('/movies', authentication); // Apply authentication middleware only to '/movies' route
 app.use('/movies', moviesRouter);
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
-
-
